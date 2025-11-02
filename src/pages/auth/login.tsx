c12b114f7b7d4fc460245c45a36b5abdb6a72e2b
Evelyn Lacerda <evelyndslacerda@gmail.com>
2025-11-02 19:50:24 -0300

    Add button to view pass

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,20 +4,8 @@ import { Button } from "@/components/ui/button";
 import { NavLink, useNavigate } from "react-router-dom";
 import api from "@/lib/api";
 import { useState } from "react";
-
-type LoginFormInputs = {
-	email: string;
-	senha: string;
-};
-
-type LoginResponse = {
-	token: string;
-	user: {
-		id: number;
-		nome: string;
-		email: string;
-	};
-};
+import type { LoginFormInputs, LoginResponse } from "@/types/Auth";
+import { Eye, EyeClosed } from "lucide-react";
 
 export default function Login() {
 	const {
@@ -33,19 +21,27 @@ export default function Login() {
 		try {
 			const response = await api.post<LoginResponse>("/login", data);
 			localStorage.setItem("token", response.data.token);
-            localStorage.setItem("hourlog:auth", JSON.stringify(response.data.user));
-            localStorage.setItem("nome", response.data.user?.nome ?? "");
+			localStorage.setItem("hourlog:auth", JSON.stringify(response.data.user));
+			localStorage.setItem("nome", response.data.user?.nome ?? "");
 			navigate("/dashboard");
 		} catch {
-			setErro("E-mail ou senha incorretos.",);
+			setErro("E-mail ou senha incorretos.");
 		}
+	};
+
+	const [showPassword, setShowPassword] = useState(false);
+
+	const showPasswordToggle = () => {
+		setShowPassword((prev) => !prev);
 	};
 
 	return (
 		<div className="w-full h-screen flex flex-col items-center justify-center">
 			<div className="flex flex-col items-center gap-0.5 bg-dark100 max-w-sm w-full py-4 rounded-t-xl">
 				<h1 className="text-xl uppercase font-bold text-orange500">Hourlog</h1>
-				<p className="text-dark700 text-sm">Faça login para acessar seus projetos.</p>
+				<p className="text-dark700 text-sm">
+					Faça login para acessar seus projetos.
+				</p>
 			</div>
 
 			<form
@@ -53,7 +49,7 @@ export default function Login() {
 				className="max-w-sm w-full flex flex-col gap-12 rounded-b-xl p-8 border-b border-x border-dashed border-dark400"
 			>
 				<div className="flex flex-col gap-6">
-					<div className="flex flex-col gap-0.5">
+					<section className="flex flex-col gap-0.5">
 						<span className="text-dark700 font-bold text-sm uppercase">
 							E-mail
 						</span>
@@ -63,22 +59,46 @@ export default function Login() {
 							placeholder="Digite seu e-mail..."
 							{...register("email", { required: "O e-mail é obrigatório" })}
 						/>
-						{errors.email && (<p className="text-red-500 text-sm font-light">{errors.email.message}</p>)}
-					</div>
+						{errors.email && (
+							<p className="text-red-500 text-sm font-light">
+								{errors.email.message}
+							</p>
+						)}
+					</section>
 
-					<div className="flex flex-col gap-0.5">
+					<section className="flex flex-col gap-0.5">
 						<span className="text-dark700 font-bold text-sm uppercase">
 							Senha
 						</span>
-						<Input
-							className="text-white"
-							type="password"
-							placeholder="Digite sua senha..."
-							{...register("senha", { required: "A senha é obrigatória" })}
-						/>
-						{errors.senha && (<p className="text-red-500 text-sm font-light">{errors.senha.message}</p>)}
-                        <NavLink to={"/recuperar"} className={"self-end text-sm text-dark700 mt-2 hover:text-orange500 transition cursor-pointer"}>Esqueceu a senha?</NavLink>
-					</div>
+						<div className="flex">
+							<Input
+								className="text-white rounded-r-none border-r-0"
+								type={showPassword ? "text" : "password"}
+								placeholder="Digite sua senha..."
+								{...register("senha", { required: "A senha é obrigatória" })}
+							/>
+							<button
+								className="rounded-r-md bg-orange500 text-dark100 px-2 cursor-pointer"
+								type="button"
+								onClick={showPasswordToggle}
+							>
+								{showPassword ? <Eye width={20} /> : <EyeClosed width={20} />}
+							</button>
+						</div>
+						{errors.senha && (
+							<p className="text-red-500 text-sm font-light">
+								{errors.senha.message}
+							</p>
+						)}
+						<NavLink
+							to={"/recuperar"}
+							className={
+								"self-end text-sm text-dark700 mt-2 hover:text-orange500 transition cursor-pointer"
+							}
+						>
+							Esqueceu a senha?
+						</NavLink>
+					</section>
 
 					{erro && <p className="text-red-500 text-sm font-light">{erro}</p>}
 				</div>
@@ -92,7 +112,13 @@ export default function Login() {
 			</form>
 
 			<span className="text-sm italic text-dark700 mt-6">
-				Não tem conta? Cadastre-se <NavLink to={"/cadastrar"} className={"font-bold hover:text-orange500 transition"} >aqui.</NavLink>
+				Não tem conta? Cadastre-se{" "}
+				<NavLink
+					to={"/cadastrar"}
+					className={"font-bold hover:text-orange500 transition"}
+				>
+					aqui.
+				</NavLink>
 			</span>
 		</div>
 	);
